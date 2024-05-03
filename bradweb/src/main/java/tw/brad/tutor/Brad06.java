18f76a04d6904751d151444e86b18c154e93191a
mickeyyo55b <mickeyyo55b@gmail.com>
2024-05-03 12:02:31 +0800

    05031202

--- bradweb/src/main/java/tw/brad/tutor/Brad06.java
+++ bradweb/src/main/java/tw/brad/tutor/Brad06.java
@@ -14,13 +14,33 @@ public class Brad06 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String span = "", x = "", y = "";
+		int result = 0;
+		String op = "";
 		
 		if (request.getParameter("x") != null) {
 			x = request.getParameter("x");
 			y = request.getParameter("y");
-			int result = Integer.parseInt(x) + Integer.parseInt(y);
+			op = request.getParameter("op");
+			switch (op) {
+				case "1":
+					result = Integer.parseInt(x) + Integer.parseInt(y);
+					span = String.format("<span>%s</span>", result+"");
+					break;
+				case "2":
+					result = Integer.parseInt(x) - Integer.parseInt(y);
+					span = String.format("<span>%s</span>", result+"");
+					break;
+				case "3":
+					result = Integer.parseInt(x) * Integer.parseInt(y);
+					span = String.format("<span>%s</span>", result+"");
+					break;
+				case "4":
+					result = Integer.parseInt(x) / Integer.parseInt(y);
+					int mod = Integer.parseInt(x) % Integer.parseInt(y);
+					span = String.format("<span>%s</span>", result+ " ... " + mod);
+					break;
+			}
 			
-			span = String.format("<span>%s</span>", result+"");
 		}
 		
 		response.setContentType("text/html; charset=UTF-8");
@@ -28,11 +48,11 @@ public class Brad06 extends HttpServlet {
 		out.append("<meta charset='UTF-8'>")
 			.append("<form>")
 			.append(String.format("<input name='x' value='%s' />", x))
-			.append(" <select>")
-			.append("<option>+</option>")
-			.append("<option>-</option>")
-			.append("<option>x</option>")
-			.append("<option>/</option>")
+			.append(" <select name='op'>")
+			.append(String.format("<option value='1' %s>+</option>", op.equals("1")?"selected":""))
+			.append(String.format("<option value='2' %s>-</option>", op.equals("2")?"selected":""))
+			.append(String.format("<option value='3' %s>x</option>", op.equals("3")?"selected":""))
+			.append(String.format("<option value='4' %s>/</option>", op.equals("4")?"selected":""))
 			.append("</select> ")
 			.append(String.format("<input name='y' value='%s' />", y))
 			.append("<input type='submit' value='='>")
