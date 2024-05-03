e33f4fb475d793b98f2dd31e2720d8d7a6dd338d
mickeyyo55b <mickeyyo55b@gmail.com>
2024-05-03 09:30:02 +0800

    05030929

---
+++ bradweb/src/main/java/tw/brad/tutor/Brad04.java
@@ -0,0 +1,50 @@
+package tw.brad.tutor;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad04")
+public class Brad04 extends HttpServlet {
+    public Brad04() {
+    	System.out.println("Brad04()");
+    }
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init(ServletConfig)");
+		//super.init(config);
+	}
+    
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init()");
+		super.init();
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) 
+			throws ServletException, IOException {
+		System.out.println("service(1)");
+		super.service(req, res);
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		System.out.println("service(2)");
+		//super.service(req, resp);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()");
+	}
+
+}
