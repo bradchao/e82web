5daa312b367391df0dcde529412c687ea9224aad
mickeyyo55b <mickeyyo55b@gmail.com>
2024-05-03 11:20:58 +0800

    05031120

--- bradweb/src/main/java/tw/brad/tutor/Brad06.java
+++ bradweb/src/main/java/tw/brad/tutor/Brad06.java
@@ -12,15 +12,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Brad06")
 public class Brad06 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String span = "", x = "", y = "";
+		
+		if (request.getParameter("x") != null) {
+			x = request.getParameter("x");
+			y = request.getParameter("y");
+			int result = Integer.parseInt(x) + Integer.parseInt(y);
+			
+			span = String.format("<span>%s</span>", result+"");
+		}
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.append("<meta charset='UTF-8'>")
 			.append("<form>")
-			.append("<input name='x' />")
-			.append("+")
-			.append("<input name='y' />")
+			.append(String.format("<input name='x' value='%s' />", x))
+			.append(" <select>")
+			.append("<option>+</option>")
+			.append("<option>-</option>")
+			.append("<option>x</option>")
+			.append("<option>/</option>")
+			.append("</select> ")
+			.append(String.format("<input name='y' value='%s' />", y))
 			.append("<input type='submit' value='='>")
+			.append(span)
 			.append("</form>");
 		response.flushBuffer();
 		
