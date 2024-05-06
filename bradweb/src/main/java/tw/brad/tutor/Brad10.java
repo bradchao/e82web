ef6af58b631344ce8b1dbd16de0054752d47c29b
mickeyyo55b <mickeyyo55b@gmail.com>
2024-05-06 12:00:18 +0800

    05061200

--- bradweb/src/main/java/tw/brad/tutor/Brad10.java
+++ bradweb/src/main/java/tw/brad/tutor/Brad10.java
@@ -3,6 +3,7 @@ package tw.brad.tutor;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,8 +15,18 @@ public class Brad10 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Brad11");
+		
+		request.setAttribute("range", 100);
+		
+		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
+		out.print("<h1>Brad Big Company</h1>");
+		out.print("<hr />");
+		out.print("<div>Hello, World</div>");
+		dispatcher.include(request, response);
+		out.print("<hr />");
 		
 		response.flushBuffer();
 	}
--- bradweb/src/main/java/tw/brad/tutor/Brad12.java
+++ bradweb/src/main/java/tw/brad/tutor/Brad12.java
@@ -9,21 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Brad11")
-public class Brad11 extends HttpServlet {
+@WebServlet("/Brad12")
+public class Brad12 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		String min = request.getParameter("min");
-		int range = (Integer)request.getAttribute("range");
+		request.setCharacterEncoding("UTF-8");
 		
 		
-		try {
-			int minValue = Integer.parseInt(min);
-			response.getWriter().print((int)(Math.random()*range+minValue));
-		}catch(Exception e) {
-			response.getWriter().print("Ooop!");
-		}
+		
+		
+
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		response.flushBuffer();
 	}
 
 }
